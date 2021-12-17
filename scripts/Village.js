68f099c2f66858b45a8dba8933aa591306f74a06
Cansu-1907 <56121553+Cansu-1907@users.noreply.github.com>
2021-12-17 23:35:10 +0100

    NewVillage page logic completed

---
+++ scripts/Village.js
@@ -0,0 +1,21 @@
+export default class Village {
+  constructor(
+    name,
+    villagers,
+    furniture,
+    wallFurniture,
+    fishes,
+    bugs,
+    sea,
+    fossils
+  ) {
+    this.name = name;
+    this.villagers = villagers;
+    this.furniture = furniture;
+    this.wallFurniture = wallFurniture;
+    this.fishes = fishes;
+    this.bugs = bugs;
+    this.sea = sea;
+    this.fossils = fossils;
+  }
+}
