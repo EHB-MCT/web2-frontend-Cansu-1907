bf184ebc6b546b39d05ae14b4c590625e5023a4a
Cansu-1907 <56121553+Cansu-1907@users.noreply.github.com>
2021-12-22 20:06:08 +0100

    Fixing problem with the loopObjAndInsertHtml function

--- src/loopObjAndInsertHtml.js
+++ src/loopObjAndInsertHtml.js
@@ -1,7 +1,13 @@
 export default function loopObjAndInsertHtml(data, container) {
   // console.log(data);
   let htmlString = "";
-  if ("villager1" in data) {
+  if (
+    "villager1" in data ||
+    "villager2" in data ||
+    "villager3" in data ||
+    "villager4" in data ||
+    "villager5" in data
+  ) {
     for (let item in data) {
       htmlString += `
         <div class="shadow"><img class="villager-img" src="${data[item]["image_uri"]}" title="${data[item].name["name-USen"]}"></div>
@@ -9,7 +15,18 @@ export default function loopObjAndInsertHtml(data, container) {
       container.innerHTML = htmlString;
     }
     container.innerHTML = htmlString;
-  } else if ("furniture1" in data || "wall-furniture1" in data) {
+  } else if (
+    "furniture1" in data ||
+    "furniture2" in data ||
+    "furniture3" in data ||
+    "furniture4" in data ||
+    "furniture5" in data ||
+    "wall-furniture1" in data ||
+    "wall-furniture2" in data ||
+    "wall-furniture3" in data ||
+    "wall-furniture4" in data ||
+    "wall-furniture5" in data
+  ) {
     for (let item in data) {
       htmlString += `
         <div class="shadow"><img src="${data[item]}"}"></div>
